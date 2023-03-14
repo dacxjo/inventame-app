feb4e729d1413f755172abe76493e61426bcf97c
David Blandón <djesus1906@protonmail.com>
2023-03-15 00:54:35 +0100

    Demo Material Design 3

--- app/src/main/java/com/ubpis/inventame/MainActivity.java
+++ app/src/main/java/com/ubpis/inventame/MainActivity.java
@@ -1,8 +1,15 @@
 package com.ubpis.inventame;
 
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
+
+
 }
